ed06135c734a473ecf0f76825eba354fa1ad1d4a
yono-ms <yono.ms@outlook.com>
2017-04-02 11:43:06 +0900

    MainViewModel

---
+++ GenericMvvm/GenericMvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMvvm
+{
+    /// <summary>
+    /// メイン画面のビューモデル
+    /// </summary>
+    [DataContract]
+    public class MainViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// ツールバータイトル
+        /// </summary>
+        [DataMember]
+        public string Title { get; set; }
+        /// <summary>
+        /// フッター
+        /// </summary>
+        [DataMember]
+        public string Fotter { get; set; }
+        /// <summary>
+        /// プログレス表示中なら真
+        /// </summary>
+        [DataMember]
+        public bool ShowProgress { get; set; }
+        /// <summary>
+        /// アプリケーション復帰中の文言
+        /// </summary>
+        [DataMember]
+        public string InitialText { get; set; }
+    }
+}
---
+++ GenericMvvm/GenericMvvm/ViewModels/MainViewModelSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMvvm
+{
+    public class MainViewModelSample : MainViewModel
+    {
+        public MainViewModelSample()
+        {
+            Title = "サンプルのタイトル";
+            Fotter = "サンプルのフッター";
+            ShowProgress = true;
+            InitialText = "ロード中です";
+        }
+    }
+}
