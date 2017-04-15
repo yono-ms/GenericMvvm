82d1fe59adac4843774050e381b6696e8cd43243
yono-ms <yono.ms@outlook.com>
2017-04-15 22:18:39 +0900

    UIスレッド指定

--- GenericMvvm/GenericMvvm.UWP/CustomFrame.cs
+++ GenericMvvm/GenericMvvm.UWP/CustomFrame.cs
@@ -16,59 +16,92 @@ namespace GenericMvvm.UWP
 
         private Image _OldImage;
 
+        private bool Waiting = false;
+
         public void Navigate(Type type, bool forward)
         {
-            // 親のGridを得る
-            var grid = Parent as Grid;
-            Task.Run(async () =>
+            try
             {
-                // 画像を作る
-                var rtb = new RenderTargetBitmap();
-                await rtb.RenderAsync(grid);
-                _OldImage = new Image();
-                _OldImage.Source = rtb;
-                //_OldImage.Width = rtb.PixelWidth;
-                //_OldImage.Height = rtb.PixelHeight;
-
-                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                Task.Run(async () =>
                 {
-                    // 作った画像を最前面に挿入
-                    grid.Children.Insert(0, _OldImage);
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                    {
+                        // 親のGridを得る
+                        var grid = Parent as Grid;
 
-                    // 画面遷移を実行
-                    Navigate(type);
+                        // 画像を作る
+                        var rtb = new RenderTargetBitmap();
+                        await rtb.RenderAsync(grid);
+                        _OldImage = new Image();
+                        _OldImage.Source = rtb;
+                        //_OldImage.Width = rtb.PixelWidth;
+                        //_OldImage.Height = rtb.PixelHeight;
+
+                        // 作った画像を最前面に挿入
+                        grid.Children.Add(_OldImage);
+
+                        // 画面遷移を実行
+                        Waiting = true;
+                        Navigate(type);
+                    });
                 });
-            });
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
         }
         protected override void OnContentChanged(object oldContent, object newContent)
         {
             base.OnContentChanged(oldContent, newContent);
             System.Diagnostics.Debug.WriteLine(FORMAT, new[] { "OnContentChanged" });
 
-            // アニメーションで古い画像を消す
-            var sb = new Storyboard();
-            var anim = new DoubleAnimationUsingKeyFrames();
-            sb.Children.Add(anim);
-            anim.KeyFrames.Add(new DiscreteDoubleKeyFrame { Value = 1, KeyTime = TimeSpan.FromSeconds(0) });
-            anim.KeyFrames.Add(new DiscreteDoubleKeyFrame { Value = 0, KeyTime = TimeSpan.FromSeconds(1) });
-            //anim.EnableDependentAnimation = true;
-            Storyboard.SetTargetProperty(anim, "Opacity");
-            Storyboard.SetTargetName(anim, "gridAnim");
-
-            sb.Completed += (s, e) =>
+            try
             {
-                // 親のGridを得る
-                var grid = Parent as Grid;
+                if (!Waiting)
+                {
+                    // 最初の画面でも来る
+                    return;
+                }
+                else
+                {
+                    Waiting = false;
+                }
 
-                // 画像を外す
-                grid.Children.Remove(_OldImage);
+                Task.Run(async () => await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    // アニメーションで古い画像を消す
+                    var sb = new Storyboard();
+                    var anim = new DoubleAnimationUsingKeyFrames();
+                    sb.Children.Add(anim);
+                    anim.KeyFrames.Add(new LinearDoubleKeyFrame { Value = 100, KeyTime = TimeSpan.FromSeconds(0) });
+                    anim.KeyFrames.Add(new LinearDoubleKeyFrame { Value = 50, KeyTime = TimeSpan.FromSeconds(1) });
+                    anim.KeyFrames.Add(new LinearDoubleKeyFrame { Value = 0, KeyTime = TimeSpan.FromSeconds(2) });
+                    //anim.EnableDependentAnimation = true;
+                    Storyboard.SetTargetProperty(anim, "Opacity");
+                    Storyboard.SetTarget(anim, _OldImage);
 
-                // 廃棄
-                _OldImage = null;
-            };
+                    sb.Completed += (s, e) =>
+                    {
+                        // 親のGridを得る
+                        var grid = Parent as Grid;
 
-            // スタート
-            sb.Begin();
+                        // 画像を外す
+                        grid.Children.Remove(_OldImage);
+
+                        // 廃棄
+                        _OldImage = null;
+                    };
+
+                    // スタート
+                    sb.Begin();
+                }));
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(FORMAT, new[] { ex.ToString() });
+            }
         }
     }
 }
