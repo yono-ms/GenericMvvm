1cb70f9bb42460410ee540538f4f6f4f9e43ad68
yono-ms <yono.ms@outlook.com>
2017-04-15 23:49:32 +0900

    スライドイン

--- GenericMvvm/GenericMvvm.UWP/CustomFrame.cs
+++ GenericMvvm/GenericMvvm.UWP/CustomFrame.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
 
@@ -14,31 +15,65 @@ namespace GenericMvvm.UWP
     {
         const string FORMAT = "----CustomFrameEvent---- {0}";
 
-        private Image _OldImage;
+        private Image _LeftImage;
+        private Image _RightImage;
 
+        /// <summary>
+        /// アニメーション用の土台
+        /// </summary>
+        private StackPanel _StackPanel;
+        /// <summary>
+        /// 画面遷移待ち
+        /// </summary>
         private bool Waiting = false;
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public CustomFrame()
+        {
+            _LeftImage = new Image();
+            _RightImage = new Image();
 
+            _StackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+            _StackPanel.Children.Add(_LeftImage);
+            _StackPanel.Children.Add(_RightImage);
+            _StackPanel.RenderTransform = new CompositeTransform();
+        }
+        /// <summary>
+        /// 進むなら真
+        /// </summary>
+        private bool Forward;
+        /// <summary>
+        /// 遷移前の画像
+        /// </summary>
+        public Image OldImage { get { return Forward ? _LeftImage : _RightImage; } }
+        /// <summary>
+        /// 遷移後の画像
+        /// </summary>
+        public Image NewImage { get { return Forward ? _RightImage : _LeftImage; } }
+        /// <summary>
+        /// 画面遷移
+        /// </summary>
+        /// <param name="type">画面</param>
+        /// <param name="forward">進むなら真</param>
         public void Navigate(Type type, bool forward)
         {
+            Forward = forward;
+
             try
             {
                 Task.Run(async () =>
                 {
                     await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
                     {
-                        // 親のGridを得る
-                        var grid = Parent as Grid;
-
                         // 画像を作る
                         var rtb = new RenderTargetBitmap();
-                        await rtb.RenderAsync(grid);
-                        _OldImage = new Image();
-                        _OldImage.Source = rtb;
-                        //_OldImage.Width = rtb.PixelWidth;
-                        //_OldImage.Height = rtb.PixelHeight;
+                        await rtb.RenderAsync(Content as Page);
+                        OldImage.Source = rtb;
 
-                        // 作った画像を最前面に挿入
-                        grid.Children.Add(_OldImage);
+                        // 土台を最前面に挿入
+                        var grid = Parent as Grid;
+                        grid.Children.Add(_StackPanel);
 
                         // 画面遷移を実行
                         Waiting = true;
@@ -68,20 +103,25 @@ namespace GenericMvvm.UWP
                     Waiting = false;
                 }
 
-                Task.Run(async () => await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                Task.Run(async () => await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
                 {
-                    // アニメーションで古い画像を消す
+                    // 画像を作る
+                    var rtb = new RenderTargetBitmap();
+                    await rtb.RenderAsync(Content as Page);
+                    NewImage.Source = rtb;
+
+                    // アニメーションで土台を移動
                     var sb = new Storyboard();
+                    var dest = (Forward ? -1 : 1) * rtb.PixelWidth;
                     var anim = new DoubleAnimation
                     {
-                        From = 1,
-                        To = 0,
-                        Duration = new Duration(TimeSpan.FromSeconds(10))
+                        From = 0,
+                        To = dest,
+                        Duration = new Duration(TimeSpan.FromMilliseconds(200))
                     };
+                    Storyboard.SetTargetProperty(anim, "(UIElement.RenderTransform).(CompositeTransform.TranslateX)");
+                    Storyboard.SetTarget(anim, _StackPanel);
                     sb.Children.Add(anim);
-                    //anim.EnableDependentAnimation = true;
-                    Storyboard.SetTargetProperty(anim, "Opacity");
-                    Storyboard.SetTarget(anim, _OldImage);
 
                     sb.Completed += (s, e) =>
                     {
@@ -89,10 +129,11 @@ namespace GenericMvvm.UWP
                         var grid = Parent as Grid;
 
                         // 画像を外す
-                        grid.Children.Remove(_OldImage);
+                        grid.Children.Remove(_StackPanel);
 
                         // 廃棄
-                        _OldImage = null;
+                        _LeftImage = null;
+                        _RightImage = null;
                     };
 
                     // スタート
