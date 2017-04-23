e1a0bb069cf612520f8211a7d6029d2fe0627a98
yono-ms <yono.ms@outlook.com>
2017-04-23 18:19:41 +0900

    Birth page

---
+++ GenericMvvm/GenericMvvm.UWP/JapaneseYearConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace GenericMvvm.UWP
+{
+    class JapaneseYearConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                var d = new DateTime((int)value, 1, 1);
+                var ci = new CultureInfo("ja-JP");
+                ci.DateTimeFormat.Calendar = new JapaneseCalendar();
+                return d.ToString("gy", ci);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
