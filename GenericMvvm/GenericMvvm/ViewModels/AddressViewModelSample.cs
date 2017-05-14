aad7e126fac1dcd66738534c076b68d3a86dfb22
yono-ms <yono.ms@outlook.com>
2017-05-14 10:37:36 +0900

    AddresPageSample

--- GenericMvvm/GenericMvvm/ViewModels/AddressViewModelSample.cs
+++ GenericMvvm/GenericMvvm/ViewModels/AddressViewModelSample.cs
@@ -14,7 +14,9 @@ namespace GenericMvvm
             PostalCode = "1234567";
             Address = "東京都八王子市ほげほげ村";
             AddressKana = "アイウエオカキクケコ";
-            var result = new ZipCloudResponse.result()
+
+            var list = new List<ZipCloudResponse.result>();
+            list.Add(new ZipCloudResponse.result()
             {
                 address1 = "東京都",
                 address2 = "八王子市",
@@ -22,10 +24,18 @@ namespace GenericMvvm
                 kana1 = "ﾄｳｷｮｳﾄ",
                 kana2 = "ﾊﾁｵｳｼﾞｼ",
                 kana3 = "ﾎｹﾞﾎｹﾞﾑﾗ"
-            };
+            });
 
-            var list = new List<ZipCloudResponse.result>();
-            list.Add(result);
+            list.Add(new ZipCloudResponse.result()
+            {
+                address1 = "東京都",
+                address2 = "八王子市",
+                address3 = "ぴよぴよ村",
+                kana1 = "ﾄｳｷｮｳﾄ",
+                kana2 = "ﾊﾁｵｳｼﾞｼ",
+                kana3 = "ﾋﾟﾖﾋﾟﾖﾑﾗ"
+            });
+
             ResponseResults = list;
 
             _Dict.Add("PostalCode", new ObservableCollection<string>(new[] { "エラーサンプル1", "サンプル2" }));
