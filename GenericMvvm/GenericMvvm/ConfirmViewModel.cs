2c36f0b03d480fde61895f2b354fef759277f270
yono-ms <yono.ms@outlook.com>
2017-05-18 08:14:01 +0900

    ConfirmViewModel

---
+++ GenericMvvm/GenericMvvm/ConfirmViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMvvm
+{
+    public class ConfirmViewModel : BaseViewModel
+    {
+        public string NameTitle { get { return "お名前"; } }
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; ValidateProperty(nameof(Name), value); }
+        }
+
+        public string BirthTitle { get { return "生年月日"; } }
+        private string _Birth;
+
+        public string Birth
+        {
+            get { return _Birth; }
+            set { _Birth = value; ValidateProperty(nameof(Birth), value); }
+        }
+
+        public string AddressTitle { get { return "住所"; } }
+        private string _Address;
+
+        public string Address
+        {
+            get { return _Address; }
+            set { _Address = value; ValidateProperty(nameof(Address), value); }
+        }
+
+        public string AddressKanaTitle { get { return "住所（ふりがな）"; } }
+        private string _AddressKana;
+
+        public string AddressKana
+        {
+            get { return _AddressKana; }
+            set { _AddressKana = value; ValidateProperty(nameof(AddressKana), value); }
+        }
+
+    }
+}
---
+++ GenericMvvm/GenericMvvm/ConfirmViewModelSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMvvm
+{
+    public class ConfirmViewModelSample : ConfirmViewModel
+    {
+        public ConfirmViewModelSample()
+        {
+            Name = "山田 太郎";
+            Birth = "2000年12月31日";
+            Address = "神奈川県横浜市神奈川区羽沢南";
+            AddressKana = "あいうえおかきくけこ";
+        }
+    }
+}
